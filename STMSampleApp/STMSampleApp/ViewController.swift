8318e9184f649b43b96472157b42bdeebc515131
Rajesh Ramachandrakurup <rajesh.ramachandrakurup@foxsports.com.au>
2020-10-20 19:18:44 +1100

    initial commit

---
+++ STMSampleApp/STMSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  STMSampleApp
+//
+//  Created by Rajesh Ramachandrakurup on 19/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ STMSampleApp/STMSampleAppTests/STMSampleAppTests.swift
@@ -0,0 +1,39 @@
+//
+//  STMSampleAppTests.swift
+//  STMSampleAppTests
+//
+//  Created by Rajesh Ramachandrakurup on 19/10/20.
+//
+
+import XCTest
+@testable import STMSampleApp
+
+class STMSampleAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testAddition() {
+        let a = 10
+        let b = 10
+        XCTAssertEqual(a+b, 20)
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
