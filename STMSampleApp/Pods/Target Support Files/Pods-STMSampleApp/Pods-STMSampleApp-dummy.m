4c2feb5263b25475b696502205dc111d5317b201
Rajesh Ramachandrakurup <rajesh.ramachandrakurup@foxsports.com.au>
2020-10-26 13:22:40 +1100

    add cocoapod

---
+++ STMSampleApp/Pods/Target Support Files/Pods-STMSampleApp/Pods-STMSampleApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_STMSampleApp : NSObject
+@end
+@implementation PodsDummy_Pods_STMSampleApp
+@end
---
+++ STMSampleApp/Pods/Target Support Files/Pods-STMSampleAppTests/Pods-STMSampleAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_STMSampleAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_STMSampleAppTests
+@end
